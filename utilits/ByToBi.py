85c843df41a0637140551d76bee64daaf562bf0d
gusarov2906 <gusarov2906@gmail.com>
2020-08-19 16:55:34 +0300

    Update utilits for new font

--- utilits/ByToBi.py
+++ utilits/ByToBi.py
@@ -1,16 +1,18 @@
 from bitstring import BitArray
 #filename = input("Write file name: ")
-filename = "font8x8.txt"
+filename = "font6x8"
 size = 8
 lines = []
 def convert(a):
     res = BitArray(hex=a)
     res = res.bin
     return res
-
+syms  = []
 with open (filename) as f:
     for line in f:
         lines.append(line.split(" //")[0])
+        if (line.split(" //")[1]):
+            syms.append((line.split(" //")[1]).split()[0])
 for i in range(len(lines)):
     lines[i] = lines[i].split(",")
 for i in range(len(lines)):
@@ -20,8 +22,11 @@ i = 0
 for line in lines:
     i+=1
     print(f'matrix {i}')
-    for sym in line:
-        print(sym)
+    for j in range(8):
+        for k in range(6):
+            print(line[k][j], end='')
+        print()
+    print()
 
 with open ("output.txt",'w') as f:
     #i=0
@@ -34,3 +39,5 @@ with open ("output.txt",'w') as f:
                 f.write(',')
             #f.write('\n')
         #f.write('\n')
+for item in syms:
+    print(f"'{item}',", end='')
