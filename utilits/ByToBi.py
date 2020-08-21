21d0977eaa1539cd8c6956b992fd225ac1c9b64b
gusarov2906 <gusarov2906@gmail.com>
2020-08-21 17:11:42 +0300

    some files for fonts

--- utilits/ByToBi.py
+++ utilits/ByToBi.py
@@ -1,8 +1,11 @@
 from bitstring import BitArray
+import numpy as np
+
 #filename = input("Write file name: ")
 filename = "font6x8"
 size = 8
 lines = []
+arrays = []
 def convert(a):
     res = BitArray(hex=a)
     res = res.bin
@@ -17,27 +20,37 @@ for i in range(len(lines)):
     lines[i] = lines[i].split(",")
 for i in range(len(lines)):
     for j in range(len(lines[i])):
-        lines[i][j] = convert(lines[i][j])
-i = 0
-for line in lines:
-    i+=1
-    print(f'matrix {i}')
-    for j in range(8):
-        for k in range(6):
-            print(line[k][j], end='')
-        print()
-    print()
+        lines[i][j] = list(convert(lines[i][j]))
 
-with open ("output.txt",'w') as f:
+i = 0
+for sym in lines:
+        arrays.append(np.rot90(np.array([sym[0],
+        sym[1],
+        sym[2],
+        sym[3],
+        sym[4],
+        sym[5]]), k = 1, axes = (0, 1)))
+
+print(arrays)
+
+
+#with open ("output.txt",'w') as f:
     #i=0
-    for line in lines:
+#    for line in lines:
         #i+=1
         #f.write(f'Str {i} \n')
-        for sym in line:
-            for item in sym:
-                f.write(item)
-                f.write(',')
+#        for sym in line:
+    #        for item in sym:
+    #            f.write(item)
+    #            f.write(',')
             #f.write('\n')
         #f.write('\n')
-for item in syms:
-    print(f"'{item}',", end='')
+#for item in syms:
+#    print(f"'{item}',", end='')
+with open ("output.txt",'w') as f:
+    for sym in arrays:
+        for line in sym:
+            for byte in line:
+                f.write(byte)
+                f.write(',')
+    f.write('\n')
---
+++ utilits/tmp1.py
@@ -0,0 +1,2 @@
+for i in range(200):
+    print(f'(unsigned char){i+128}', end=", ")
